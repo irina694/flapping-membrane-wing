6d3e3df3563f6db333e7218a5c3cd8d651032dc8
Irina Patrikeeva <irina@Goldberry.(none)>
2011-07-12 17:01:54 -1000

    deleted windows application files

--- flapping_membrane_win/distrib/_install.bat
+++
@@ -1,8 +0,0 @@
-echo off
-echo Deploying project flapping_membrane_win.
-IF EXIST MCRInstaller.exe (
-echo Running MCRInstaller
-MCRInstaller.exe
-)
-echo Installation complete.
-echo Please refer to the documentation for any additional setup steps.
